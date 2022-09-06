57320e7f94c3a2de0d9f69a68d0bca300819848f
Arthur Cesar Araújo <58611857+ArthurCesarDev@users.noreply.github.com>
2022-09-06 09:27:35 -0300

    Update register.blade.php

--- resources/views/admin/register.blade.php
+++ resources/views/admin/register.blade.php
@@ -28,7 +28,7 @@ em 30/09/2022, a proposta de incorporação societária da rede Hortifruti Natur
 e lojas forem sendo divulgados.</p>
 
 <br>
-<p class="text-center">Solicitamos sua confirmação para essa nova Etapa! para confirmar que recebeu o 
+<p class="text-center font-weight-bold">Solicitamos sua confirmação para essa nova Etapa! para confirmar que recebeu o 
 comunicado e alterou o cadastro do Hortifruti Natural da Terra em seus sistemas preenchendo os campos abaixo. Juntos somos mais fortes! </p>
 
 
